e5fef5f988e8e83efa6528e2c86fe0270e733e34
bert van eeckhoutte <bert.vaneeckhoutte@student.hogent.be>
2020-11-20 16:34:24 +0100

    some ktlint refactoring MainActivity.kt

--- Dropspot/app/src/main/java/com/example/dropspot/MainActivity.kt
+++ Dropspot/app/src/main/java/com/example/dropspot/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.dropspot
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.security.keystore.KeyGenParameterSpec
@@ -29,7 +28,6 @@ import com.example.dropspot.fragments.HomeFragmentDirections
 import com.example.dropspot.utils.Constants.AUTH_ENC_SHARED_PREF_KEY
 import com.example.dropspot.utils.Variables
 import com.example.dropspot.viewmodels.UserViewModel
-import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
@@ -41,17 +39,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private val userViewModel: UserViewModel by viewModel()
     lateinit var binding: ActivityMainBinding
 
-    //nav
-    private lateinit var toolbar: MaterialToolbar
+    // nav
     private lateinit var drawerLayout: DrawerLayout
-    private lateinit var navView: NavigationView
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navController: NavController
 
     companion object {
         private const val TAG = "main_activity"
     }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,37 +59,43 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     private fun setupLoggedInUser() {
         userViewModel.setSessionToken(this.intent.getStringExtra("TOKEN")!!)
-        userViewModel.isSessionExpired.observe(this, Observer {
-            if (it) {
-                showSessionExpiredAndLogout()
+        userViewModel.isSessionExpired.observe(
+            this,
+            Observer {
+                if (it) showSessionExpiredAndLogout()
             }
-        })
+        )
         userViewModel.fetchUser()
     }
 
     private fun setupObservers() {
         // update side nav username
-        userViewModel.currentUser.observe(this, Observer {
-            if (it != null) {
-                Log.i(TAG, "fetched user: $it")
-                binding.loggedInUser = it
+        userViewModel.currentUser.observe(
+            this,
+            Observer {
+                if (it != null) {
+                    Log.i(TAG, "fetched user: $it")
+                    binding.loggedInUser = it
+                }
             }
-        })
+        )
 
         // handles connection response
-        Variables.isNetworkConnected.observe(this, Observer {
-            if (it) {
-                if (userViewModel.currentUser.value == null) {
-                    userViewModel.fetchUser()
+        Variables.isNetworkConnected.observe(
+            this,
+            Observer {
+                if (it) {
+                    if (userViewModel.currentUser.value == null) {
+                        userViewModel.fetchUser()
+                    }
+                    binding.animNoConnection.visibility = View.INVISIBLE
+                } else {
+                    binding.animNoConnection.visibility = View.VISIBLE
+                    binding.animNoConnection.bringToFront()
                 }
-                binding.animNoConnection.visibility = View.INVISIBLE
-            } else {
-                binding.animNoConnection.visibility = View.VISIBLE
-                binding.animNoConnection.bringToFront()
             }
-        })
+        )
     }
-
 
     private fun setupNav() {
         val navHostFragment =
@@ -103,15 +103,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         navController = navHostFragment.navController
         navController.setGraph(R.navigation.navigation)
         drawerLayout = binding.drawerLayout
-        toolbar = binding.toolbar
-        navView = binding.navView
-        appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        val toolbar = binding.toolbar
+        val navView = binding.navView
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         toolbar.setupWithNavController(navController, appBarConfiguration)
         // prevent nav gesture if not on start destination
         navController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _ ->
             if (nd.id == nc.graph.startDestination) {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-
             } else {
                 drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
             }
