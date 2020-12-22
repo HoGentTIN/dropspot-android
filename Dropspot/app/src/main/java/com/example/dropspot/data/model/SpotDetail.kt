4b43ae2da66a364e83d8d82b11d26f11347b557c
bert van eeckhoutte <bert.vaneeckhoutte@student.hogent.be>
2020-12-22 16:58:37 +0100

    improved location representation and added unittests

--- Dropspot/app/src/main/java/com/example/dropspot/data/model/SpotDetail.kt
+++ Dropspot/app/src/main/java/com/example/dropspot/data/model/SpotDetail.kt
@@ -6,6 +6,9 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
+import kotlin.math.abs
+import kotlin.math.floor
+import kotlin.math.round
 import kotlin.math.roundToInt
 
 @Parcelize
@@ -28,12 +31,41 @@ data class SpotDetail(
 ) : Parcelable {
 
     fun getLocationString(): String {
-        if (address == null) {
-            return "LAT: $latitude\nLONG: $longitude"
+        return if (address == null) {
+            latLngToDMS(latitude,longitude)
         } else {
-            return address!!.getAddressString()
+            address!!.getAddressString()
         }
     }
+
+    private fun latLngToDMS(lat : Double, lng : Double) : String{
+        var latString : String = if (lat >= 0) "N" else "S"
+        var lngString : String = if (lng >= 0) "E" else "W"
+
+        latString += calcDMS(lat)
+        lngString += calcDMS(lng)
+
+        return "$latString\n$lngString"
+    }
+
+    private fun calcDMS(doubleCoordinate : Double): String {
+        val absValue = abs(doubleCoordinate)
+
+        val d = floor(absValue)
+        val m = floor((absValue - d) * 60)
+        val s = round((absValue - d - m / 60) * 3600 * 1000) / 1000
+
+        var resD : Any = d
+        var resM : Any = m
+        var resS : Any = s
+
+        if (d.toInt() - d == 0.0) resD = d.toInt()
+        if (m.toInt() - m == 0.0) resM = m.toInt()
+        if (s.toInt() - s == 0.0) resS = s.toInt()
+
+        return "$resD°$resM'$resS\""
+    }
+
 
     fun isPark(): Boolean {
         return address != null
--- Dropspot/app/src/test/java/com/example/dropspot/SpotDetailTest.kt
+++ Dropspot/app/src/test/java/com/example/dropspot/SpotDetailTest.kt
@@ -100,4 +100,100 @@ class SpotDetailTest {
         assertThat(spotDetail.getSaveSliderValueForEntranceFee()).isEqualTo(expectedValue)
     }
 
+    @Test
+    fun latLngToDMS_NW(){
+        val inputLat = 43.068888
+        val inputLng = -111.972656
+        val expectedDMSString = "N43°4'7.997\"\nW111°58'21.562\""
+
+        val spotDetail = SpotDetail(
+            0,
+            "test",
+            0,
+            "test",
+            inputLat,
+            inputLng,
+            null,
+            ArrayList(),
+            false,
+            false,
+            null,
+            null
+        )
+
+        assertThat(spotDetail.getLocationString()).isEqualTo(expectedDMSString)
+    }
+
+    @Test
+    fun latLngToDMS_NE(){
+        val inputLat = 42.032974
+        val inputLng = 74.707031
+        val expectedDMSString = "N42°1'58.706\"\nE74°42'25.312\""
+
+        val spotDetail = SpotDetail(
+            0,
+            "test",
+            0,
+            "test",
+            inputLat,
+            inputLng,
+            null,
+            ArrayList(),
+            false,
+            false,
+            null,
+            null
+        )
+
+        assertThat(spotDetail.getLocationString()).isEqualTo(expectedDMSString)
+    }
+
+    @Test
+    fun latLngToDMS_SE(){
+        val inputLat = -27.683528
+        val inputLng = 119.707031
+        val expectedDMSString = "S27°41'0.701\"\nE119°42'25.312\""
+
+        val spotDetail = SpotDetail(
+            0,
+            "test",
+            0,
+            "test",
+            inputLat,
+            inputLng,
+            null,
+            ArrayList(),
+            false,
+            false,
+            null,
+            null
+        )
+
+        assertThat(spotDetail.getLocationString()).isEqualTo(expectedDMSString)
+    }
+
+    @Test
+    fun latLngToDMS_SW(){
+        val inputLat = -34.885931
+        val inputLng = -64.863281
+        val expectedDMSString = "S34°53'9.352\"\nW64°51'47.812\""
+
+        val spotDetail = SpotDetail(
+            0,
+            "test",
+            0,
+            "test",
+            inputLat,
+            inputLng,
+            null,
+            ArrayList(),
+            false,
+            false,
+            null,
+            null
+        )
+
+        assertThat(spotDetail.getLocationString()).isEqualTo(expectedDMSString)
+    }
+
 }
